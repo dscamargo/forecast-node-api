09963c84747ee9fbb194dde2b650684eaa5835da
Douglas S. Camargo <douglas@nibbletecnologia.com.br>
2020-08-20 23:24:10 -0300

    CI/CD prd

--- src/database.ts
+++ src/database.ts
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 
 const dbConfig: IConfig = config.get('App.database');
 
+console.log(dbConfig.get('mongoUrl'))
+
 export const connect = async (): Promise<typeof mongoose> =>
   await mongoose.connect(dbConfig.get('mongoUrl'), {
     useCreateIndex: true,
