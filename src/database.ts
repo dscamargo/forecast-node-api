b9979b850f9f7a59dfc32a5cad3b3f90dd6839ea
Douglas S. Camargo <douglas@nibbletecnologia.com.br>
2020-08-21 13:38:04 -0300

    CI

--- src/database.ts
+++ src/database.ts
@@ -3,15 +3,15 @@ import mongoose from 'mongoose';
 
 const dbConfig: IConfig = config.get('App.database');
 
-
-console.log(dbConfig.get('mongoUrl'))
 export const connect = async (): Promise<typeof mongoose> => {
   const connection = await mongoose.connect(dbConfig.get('mongoUrl'), {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-  console.log('Database connected')
+  if (process.env.NODE_ENV !== 'test'){
+    console.log('Database connected')
+  }
   return connection
 }
 
